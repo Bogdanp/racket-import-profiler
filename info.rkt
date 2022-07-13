aada80a7816ea8d7c19a776d7d7b0f939f0c2996
Bogdan Popa <bogdan@defn.io>
2022-07-13 10:40:03 +0300

    build: add license field

--- info.rkt
+++ info.rkt
@@ -5,3 +5,4 @@
 (define deps '("base"))
 (define raco-commands
   '(("profile-imports" (submod pkg/import-profiler main) "profile module dependency import times" #f)))
+(define license 'BSD-3-Clause)
