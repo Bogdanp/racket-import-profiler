9cd789780cf811c51455e53b7c26949780540178
Bogdan Popa <bogdan@defn.io>
2022-12-11 18:59:57 +0200

    build: remove duplicate license entry

--- info.rkt
+++ info.rkt
@@ -6,4 +6,3 @@
 (define deps '("base"))
 (define raco-commands
   '(("profile-imports" (submod pkg/import-profiler main) "profile module dependency import times" #f)))
-(define license 'BSD-3-Clause)
